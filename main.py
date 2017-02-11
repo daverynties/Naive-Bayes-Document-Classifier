a7a629ba6fd2b8361e730d9a81957f8a2edf9e42
David Rynties <ryntiesd@mail.gvsu.edu>
2017-02-11 18:23:57 -0500

    incomplete, semi-functionality -- must fix classifier verification and word check

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 from collections import defaultdict
 from collections import Counter
 from itertools import chain
-
+from math import log
 #==============================================================================
 # ###TRAINING
 # 
 #==============================================================================
 totalTrainingDocs = 0
 
-d = defaultdict(list)
-my_dict = defaultdict(int)
+class_word_prob = defaultdict(list)
+class_doc_length = defaultdict(int)
 vocabDicts = {}
 
 with open('trainingData.txt') as f:
     for line in f.readlines():
-        my_dict[(line.split(' ', 1)[0])] += 1
+        class_doc_length[(line.split(' ', 1)[0])] += 1
         data = line.split()
-        d[data[0]].extend(data[1:])
+        class_word_prob[data[0]].extend(data[1:])
         totalTrainingDocs += 1
              
 #get total unique words in all documents -- Vocabulary        
-Vocabulary = list(set(list(chain.from_iterable(d.values()))))
+Vocabulary = list(set(list(chain.from_iterable(class_word_prob.values()))))
 
 #convert list to dictionary to remove duplicate words and get count
-for key in d:
-    d[key] = Counter(d[key])
+for key in class_word_prob:
+    class_word_prob[key] = Counter(class_word_prob[key])
     
 #calc probability function      
 def calculateProbability(cls, word):
     
     vocabLength = len(Vocabulary)
-    wordOccurance = d[cls][word]
-    classLength = len(d[cls])
+    wordOccurance = class_word_prob[cls][word]
+    classLength = len(class_word_prob[cls])
     
     #calculate Probability
     probability = ((wordOccurance + 1) / (float(classLength) + vocabLength))    
@@ -39,16 +39,47 @@ def calculateProbability(cls, word):
 #for every word in vocabulary, check occurances of each class and set value 
 #of current dictionary        
 for word in Vocabulary:      
-    for cls in d:
-        if word in d[cls]:
-            d[cls][word] = calculateProbability(cls, word)
+    for cls in class_word_prob:
+        if word in class_word_prob[cls]:
+            class_word_prob[cls][word] = calculateProbability(cls, word)
             
 #==============================================================================
 # ###_CLASSIFICATION__###
 # 
 #==============================================================================
-#class_word = 'test'
-#class_value = 0.0
+class_word = 'test'
+correct_classification = []
+max_sum_class = -10000000.00
+
+test_class_word_prob = defaultdict(list)
+
+with open('testData.txt') as f:
+    for line in f.readlines():      
+        #class_doc_length[(line.split(' ', 1)[0])] += 1
+        testData = line.split()
+        #grab first word for future comparison
+        cls_value = testData.pop(0)
+        #loop through classes for each word
+        for cls in class_word_prob:
+            word_probability_values = []
+            for word in testData:
+                if word in class_word_prob[cls]:
+                     word_probability_values.append(log(class_word_prob[cls][word]))
+                     print("true")
+                else:
+                    word_probability_values.append(log(1 / float(class_doc_length[cls])))
+            if sum(word_probability_values) > max_sum_class:
+                max_sum_class = sum(word_probability_values)
+                class_word = cls
+    if cls_value == class_word:    
+        correct_classification.append("TRUE")
+    else:
+        correct_classification.append("FALSE")
+    #correct_classification = list(set(correct_classification))
+        
+           
+
+
 
 #create nested dic of test docs to get unique words and count
 #count will be overrided to probability value for each class
